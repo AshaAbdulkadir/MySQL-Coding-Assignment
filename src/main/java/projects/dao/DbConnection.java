8a5596a048790a1a56ef2484dcbeb2f255837597
AshaAbdu <halge87@yahoo.com>
2022-08-30 18:09:03 -0500

    3rd commit

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+/**
+ * @author Asha
+ *
+ */
+public class DbConnection {
+	private static String HOST = "localhost";
+	private static String PASSWORD = "projects";
+	private static int PORT = 3306;
+	private static String SCHEMA = "projects";
+	private static String USER = "projects";
+	
+	public static Connection getConnection() {
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER, PASSWORD);
+		System.out.println("Connect with url =" + url);
+		
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("Connection successful!");
+			return conn;
+		} catch (SQLException e) {
+			throw new DbException(e);
+		}
+		
+	}
+
+}
