b046b0e4ddc75d403604f93202ff598ba0ce56f9
AshaAbdu <halge87@yahoo.com>
2022-09-07 05:15:58 -0500

    2nd commit

--- src/main/resources/projects-schema.sql
+++ src/main/resources/projects-schema.sql
@@ -6,31 +6,42 @@ DROP TABLE IF EXISTS project_category;
 
 
 CREATE TABLE project (
-	project_id INT NOT NULL,
+	project_id INT AUTO_INCREMENT NOT NULL,
 	project_name VARCHAR(128) NOT NULL,
 	estimated_hours TIME,
 	actual_hours TIME,
 	difficulty INT NOT NULL,
-	notes TEXT
+	notes TEXT,
+	PRIMARY KEY (project_id)
 );
 CREATE TABLE material (
-	material_id INT NOT NULL,
+	material_id INT AUTO_INCREMENT NOT NULL,
 	project_id INT NOT NULL,
 	material_name VARCHAR(128) NOT NULL,
 	num_required INT NULL,
-	cost DECIMAL(7,2)
+	cost DECIMAL(7,2),
+	PRIMARY KEY (material_id),
+	FOREIGN KEY (project_id) REFERENCES project (project_id) 
 );
-CREATE TABLE step (
-	step_id INT NOT NULL,
+CREATE TABLE steps (
+	step_id INT AUTO_INCREMENT NOT NULL,
 	project_id INT NOT NULL,
 	step_text TEXT NOT NULL,
-	step_order INT NOT NULL
+	step_order INT NOT NULL,
+	PRIMARY KEY (step_id),
+	FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
 CREATE TABLE category (
-	category_id INT NOT NULL,
-	category_name VARCHAR(128) NOT NULL
+	category_id INT AUTO_INCREMENT NOT NULL,
+	category_name VARCHAR(128) NOT NULL,
+	PRIMARY KEY (category_id)
 );
 CREATE TABLE project_category (
-	project_id INT NOT NULL,
-	category_id INT NOT NULL
+	project_id INT AUTO_INCREMENT NOT NULL,
+	category_id INT AUTO_INCREMENT NOT NULL
+	PRIMARY KEY (project_id, category_id),
+	FOREIGN KEY (project_id, category_id),
+	FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
+	FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
+	UNIQUE KEY (project_id, category_id)
 );
