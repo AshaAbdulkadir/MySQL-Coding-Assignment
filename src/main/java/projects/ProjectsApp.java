9f0a99fec3c3d0648e76478205c5f3c2e548ec82
AshaAbdu <halge87@yahoo.com>
2022-09-11 18:40:46 -0500

    Working with BigDecimal in Java to handle the SQL DECIMAL - week9

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,14 +1,17 @@
 package projects;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
+import projects.entity.Project;
 import projects.exception.DbException;
+import projects.service.ProjectService;
 
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
-	
+	private ProjectService projectService = new ProjectService();
 	
 	// @formatter: off
 	private List<String> operations = List.of(
@@ -52,13 +55,42 @@ public class ProjectsApp {
 	}
 
 	private void createProject() {
-		// TODO Auto-generated method stub
+		String projectName = getStringInput("Enter the project name");
+		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
+		BigDecimal actualHours = getDecimalInput ("Enter the actual hours");
+		Integer difficulty = getIntInput("Enter the project difficulty(1-5)");
+		String notes = getStringInput("Enter the project notes");
 		
+		Project project = new Project();
+		
+		project.setProjectName(projectName);
+		project.setEstimatedHours(estimatedHours);
+		project.setActualHours(actualHours);
+		project.setDifficulty(difficulty);
+		project.setNotes(notes);
+		
+		Project dbProject = projectService.addProject(project);
+		System.out.println("You have successfully created project: " + dbProject);
+	}
+
+	private BigDecimal getDecimalInput(String prompt) {
+		String input = getStringInput(prompt);
+		
+		if (Objects.isNull(input)) {
+		return null;	
+		}
+		
+		try {
+			return new BigDecimal(input).setScale(2);
+		}
+		catch(NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number.");
+		}
 	}
 
 	private boolean exitMenu() {
-		// TODO Auto-generated method stub
-		return false;
+		System.out.println("Exiting the menu.");
+		return true;
 	}
 
 	private int getUserSelection() {
@@ -69,7 +101,7 @@ public class ProjectsApp {
 	}
 
 	private Integer getIntInput(String prompt) {
-		String input = getstringInput(prompt);
+		String input = getStringInput(prompt);
 		
 		if (Objects.isNull(input)) {
 			return null;
@@ -83,7 +115,7 @@ public class ProjectsApp {
 		}
 	}
 
-	private String getstringInput(String prompt) {
+	private String getStringInput(String prompt) {
 		System.out.println(prompt + ": ");
 		String input = scanner.nextLine();
 		
---
+++ src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package projects.service;
+
+import projects.entity.Project;
+
+/**
+ * @author Asha
+ *
+ */
+public class ProjectService {
+
+	public Project addProject(Project project) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
