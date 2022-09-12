39b8abe7a73fa7d782e34f078563ba91af79c919
AshaAbdu <halge87@yahoo.com>
2022-09-12 01:35:26 -0500

    modification to menu app

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -12,11 +12,13 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	
 	// @formatter: off
 	private List<String> operations = List.of(
 			"1) Add a project",
-			"2) List projects"
+			"2) List projects",
+			"3) Select a project"
 			
 	);
 	// @formatter:on
@@ -47,6 +49,10 @@ public class ProjectsApp {
 					case 2:
 						listProjects();
 						break;
+						
+					case 3:
+						selectProject();
+						break;
 					
 					default:
 						System.out.println("\n" + selection + " is not a valid selection. Try again.");
@@ -57,6 +63,12 @@ public class ProjectsApp {
 				System.out.println("\nError: " + e + " Try again.");
 			}
 		}
+	}
+
+	private void selectProject() {
+		listProjects();
+		Integer projectId = getIntInput("Enter a project ID to select a project");
+		
 	}
 
 	private void listProjects() {
